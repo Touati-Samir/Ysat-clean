5136e3999ff3873183d32c5c4157db72b8c613a6
Samir <samir_touati@icloud.com>
2024-11-22 15:40:48 +0100

    Mise à jour finale avec toutes les pages et styles

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// JavaScript pour Ysat Clean
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Le script fonctionne!");
+});
+function toggleMenu() {
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.toggle("active");
+}
